91dcd826987903376ba42e47e42bdddeca6d7a56
Jsakkos <jonathansakkos@protonmail.com>
2021-09-08 13:45:55 -0400

    Update make-tarball.py

--- make-tarball.py
+++ make-tarball.py
@@ -1,4 +1,10 @@
 import os
 import shutil
 from pathlib import Path
+import tarfile
+tar = tarfile.open("test.tar.gz",mode='r')
+tar.extractall()
+tar.close()
 files = sorted(Path('.').glob('**/*.h5'))
+for file in files:
+    
