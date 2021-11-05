6a5a8c1cd9e938ef77a9fc5e5f6f9832f83a7718
Jsakkos <jonathansakkos@protonmail.com>
2021-11-05 11:34:25 -0400

    Update se-opt.py

--- scripts/se-opt.py
+++ scripts/se-opt.py
@@ -101,21 +101,6 @@ def func(x):
     
     return mean_squared_error(df.OD750,test_data.OD750, squared = False) + mean_squared_error(df.Sucrose,test_data.Sucrose, squared = False)
 
-
-
-""" alpha_min = float('-1e0')
-alpha_max = float('0')
-tau_min = float('0')
-tau_max = float('1e0')
-c_min = float('-1e1')
-c_max = float('5e-1')
-alpha2_min = float('-1e1')
-alpha2_max = float('1e-1')
-tau2_min = float('0')
-tau2_max = float('1e0')
-c2_min = float('0')
-c2_max = float('1e1') """
-
 alpha_min = float('-2e-1')
 alpha_max = float('0')
 tau_min = float('1e-2')
@@ -141,5 +126,57 @@ n_calls = 15
 
 res = gp_minimize(func, bounds, n_calls=n_calls,verbose=True,
                   callback=[checkpoint_saver])
+
+# plot
+
+alpha = res[0]
+tau = res[1]
+c = res[2]
+#Sucrose fitting
+alpha2 = res[3]
+tau2 = res[4]
+c2 = res[5]
+#growth rate
+mu = res[6]
+os.chdir('/lustre/or-scratch/cades-cnms/12x/nufeb-test')
+recompile(alpha,tau,c,alpha2,tau2,c2)
+print(f'alpha: {alpha},tau: {tau},c: {c},alpha2: {alpha2},tau2: {tau2},c2: {c2}, mu: {mu}')
+
+#Clean old simulations
+os.system('nufeb-clean')
+#Run simulation
+for iptg in test_data.IPTG:
+    text = f'nufeb-seed --cells 10,0 --d 1e-4,1e-4,1e-5 --t 8700 --mucya {mu} --sucR {iptg}'
+    os.system(text)
+
+run_nufeb = subprocess.run('/home/12x/optim3.sh', stdout=subprocess.DEVNULL)
+BASE_DIR = Path(f'runs/')
+folders = [path for path in BASE_DIR.iterdir() if path.is_dir()]
+
+#Extract output
+
+data = [utils.get_data(directory=str(x)) for x in folders]
+Volume = 1e-4*1e-4*1e-4 #m^3
+CellNum2OD = Volume*1e6/0.3e-8
+SucroseMW = 342.3
+dfs = []
+for x in data:
+    temp = pd.concat([x.ntypes.cyano/CellNum2OD,x.ntypes.step/60/60*x.timestep,x.avg_con.Sucrose.reset_index(drop=True)],axis=1)
+    temp.columns=['OD750','Hours','Sucrose']
+    temp['IPTG'] = x.metadata['SucRatio']
+    dfs.append(temp)
+df = pd.concat(dfs,ignore_index=True)
+df = df.loc[(df.Hours > 23.8) & (df.Hours < 24)]
+df.sort_values(by='IPTG',inplace=True)
+df.reset_index(inplace=True)
+
+f, ax = plt.subplots(ncols=2)
+ax[0].set_title('Sucrose')
+ax[0].plot(test_data.IPTG,test_data.Sucrose,marker='o')
+ax[0].plot(df.IPTG,df.Sucrose)
+ax[1].set_title('OD750')
+ax[1].plot(test_data.IPTG,test_data.OD750,marker='o')
+ax[1].plot(df.IPTG,df.OD750)
+f.savefig(r'../figures/se-opt.png')
 
 
