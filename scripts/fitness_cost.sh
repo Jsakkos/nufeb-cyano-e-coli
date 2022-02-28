b11de47eb0a866b26aed0245da4986ea5deb60ff
Jsakkos <jonathansakkos@protonmail.com>
2022-02-28 16:14:45 -0500

    Made fitness cost scripts

---
+++ scripts/fitness_cost.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+module load libpng
+export LAMMPS=~/NUFEB/lammps/src/lmp_png
+srun="srun -N1 -n1 --mpi=pmi2 --mem=1g --time 3:30:00"
+cd /mnt/gs18/scratch/users/sakkosjo/fitness-cost
+for i in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
+    for j in 0.5 1 2 3 4 5; do
+        python ~/nufeb-cyano-e-coli/scripts/fitness_params.py -f $i -s $j
+        ./install.sh --enable-hdf5
+        nufeb seed --n 3
+        for dir in runs/*/; do
+            cd "$dir"
+            $srun $LAMMPS -in *.lammps > nufeb.log &
+            cd /mnt/gs18/scratch/users/sakkosjo/fitness-cost
+        done
+        wait
+        mv runs runs_${i}_${j}
+        echo "fitness $i, biomass $j done"
+    done
+done
---
+++ scripts/fitness_params.py
@@ -0,0 +1,56 @@
+import argparse
+import sys
+from pathlib import Path
+import os
+from string import Template
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+        args (List[str]): command line parameters as list of strings
+            (for example  ``["--help"]``).
+
+    Returns:
+        :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    # arguments to modify the conditions of the simulation seeding
+    parser = argparse.ArgumentParser(description="Create atom definition files")
+    parser.add_argument(
+        "-f",
+        dest="fitness",
+        action="store",
+        default=1,
+        type=float,
+    )
+    parser.add_argument(
+        "-s",
+        dest="sucrose",
+        action="store",
+        default=4,
+        type=float,
+    )
+    return parser.parse_args(args)
+def main(args):
+    """Wrapper function to generate new NUFEB simulation conditions
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+
+    """
+    args = parse_args(args)
+    filein = open( f'/mnt/home/sakkosjo/nufeb-cyano-e-coli/templates/fix_bio_kinetics_monod_fitness.txt' )
+    src = Template( filein.read() )
+    result = src.safe_substitute({'fitness' : args.fitness, 'biomass_flux' : args.sucrose})
+    with open(f"/mnt/gs18/scratch/users/sakkosjo/fitness-cost/src/USER-NUFEB/fix_bio_kinetics_monod.cpp","w") as f:
+        f.writelines(result)
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+
+    run()
