69c29397e7faf5433c8ac1617a2b40ce0ceb83f4
Jsakkos <jonathansakkos@protonmail.com>
2022-03-14 10:11:04 -0400

    Update se-opt-ICER.py

--- scripts/se-opt-ICER.py
+++ scripts/se-opt-ICER.py
@@ -165,12 +165,28 @@ c2_max = float('1e1')
 bounds = [(alpha_min,alpha_max),(tau_min,tau_max),(c_min,c_max),(alpha2_min,alpha2_max),(tau2_min,tau2_max),(c2_min,c2_max)]#,
 
 
+if Path('/mnt/home/sakkosjo/nufeb-cyano-e-coli/checkpoints/checkpoint-se-icer.pkl').exists():
+    res = load('/mnt/home/sakkosjo/nufeb-cyano-e-coli/checkpoints/checkpoint-se-icer.pkl')
+    checkpoint_saver = CheckpointSaver('/mnt/home/sakkosjo/nufeb-cyano-e-coli/checkpoints/checkpoint-se-icer.pkl', compress=9)
+    n_calls = 500
 
-checkpoint_saver = CheckpointSaver('/mnt/home/sakkosjo/nufeb-cyano-e-coli/checkpoints/checkpoint-se-icer.pkl', compress=9)
-n_calls = 500
+    Nfeval = len(res.x_iters)+1
+    x0 = res.x_iters
+    y0 = res.func_vals
+    #base_estimator = res.specs['args']['base_estimator']
+    random_state = res.random_state
+    res = gp_minimize(func, bounds, x0=x0,y0=y0,
+            n_calls=n_calls,n_initial_points=20,
+            verbose=True,random_state=random_state,
+                    callback=[checkpoint_saver])
+else:
+    checkpoint_saver = CheckpointSaver('/mnt/home/sakkosjo/nufeb-cyano-e-coli/checkpoints/checkpoint-se-icer.pkl', compress=9)
+    n_calls = 500
 
-res = gp_minimize(func, bounds, n_calls=n_calls,verbose=True,
-                  callback=[checkpoint_saver])
+    Nfeval = 1
 
+    res = gp_minimize(func, bounds,
+            n_calls=n_calls,n_initial_points=20,
+            verbose=True, callback=[checkpoint_saver])
 
 
